9879f4df1f81856af6aa191778071b80e1850b6a
ruaou <211989281@qq.com>
2025-03-10 19:33:18 +0800

    无涯英文修改
    Signed-off-by: ruaou <211989281@qq.com>

--- src/.config/default.ts
+++ src/.config/default.ts
@@ -6,7 +6,7 @@ import type { ThemeConfig } from '~/types'
 export const defaultConfig: ThemeConfig = {
   site: {
     title: '无涯集',
-    subtitle: 'The Boundless Archive',
+    subtitle: 'Wuya Episteme',
     author: 'Kira',
     description: 'Rediscory the beauty of typography',
     website: 'https://astro-theme-typography.vercel.app/',
